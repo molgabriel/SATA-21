67ac322ed247466f0fb4a4506c10d95f1917c874
Molnár Gabriella <molgabriel@hotmail.com>
2017-03-08 21:38:14 +0100

    A feladat elvégezve.

--- sata21/main.c
+++ sata21/main.c
@@ -3,5 +3,28 @@
 
 int main()
 {
+    int array[100];
+    int i;
+    int number=100;
+    int sum=0;
+    for(i=0;i<100;number++){
+        if((0==(number%5)) && (0==(number%7))){
+            array[i++]=number;
+            printf("%d\t", number);
+        }
+    }
+
+    for(i=1;i<100;i+=2){
+        sum+=array[i];
+    }
+    printf("\n%d", sum);
+
+    sum=0;
+    for(i=0;i<100;i+=2){
+        sum+=array[i];
+    }
+    sum/=50;
+    printf("\n%d", sum);
+
     return 0;
 }
