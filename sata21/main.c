5f4f170eb3cac58ba886f3f93c0fea3ebf28bec9
Nagy Zsolt <stay.pista@gmail.com>
2017-03-09 08:56:51 +0100

    Kommentek hozzáadása
    Kommentek és feladat kiírás hozzáadva.

--- sata21/main.c
+++ sata21/main.c
@@ -1,26 +1,43 @@
+/**
+ * main.c
+ *
+ * Created: 2017. 03. 08. 20:47:32
+ * Author : Nagy Zsolt
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/** Feladat: http://uni-obuda.hu/users/sandor.tamas/Programozas1_Informatika_labor/feladatok_2012_04_22.pdf
+21. feladat
+*/
+
+
 int main()
 {
+
     int array[100];
     int i;
     int number=100;
     int sum=0;
-    for(i=0;i<100;number++){
-        if((0==(number%5)) && (0==(number%7))){
+    for(i=0; i<100; number++)
+    {
+        if((0==(number%5)) && (0==(number%7)))
+        {
             array[i++]=number;
             printf("%d\t", number);
         }
     }
 
-    for(i=1;i<100;i+=2){
+
+    for(i=1; i<100; i+=2)
+    {
         sum+=array[i];
     }
     printf("\n%d", sum);
 
-    sum=0;
-    for(i=0;i<100;i+=2){
+
+    for(i=0, sum=0; i<100; i+=2)
+    {
         sum+=array[i];
     }
     sum/=50;
